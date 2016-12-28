9f526d84e01fccbeadb956bbea8c8d36ab1d1157
Nicolas Pucci <nicolaspucci1989@gmail.com>
2016-12-28 19:29:04 -0300

    nada

--- funciones.c
+++ funciones.c
@@ -1,10 +1,14 @@
 // Nuevo indice provincial para cada provincial
 #include "header_funciones.h"
+#include "estructuras_datos.h"
+
 
 void nuevoIndiceProvincial(float indicesDeProvinciaPorRubro[CANTIDAD_PROVINCIAS][CANTIDAD_RUBROS])
 {
   int i, j;
   float acumulador[CANTIDAD_PROVINCIAS] = {0};
+
+  void imprimirAcumulador(float acumulador[CANTIDAD_PROVINCIAS]);
 
   for(i = 0; i < CANTIDAD_PROVINCIAS; i++){
     for(j = 0; i < CANTIDAD_RUBROS; j++){
@@ -15,4 +19,16 @@ void nuevoIndiceProvincial(float indicesDeProvinciaPorRubro[CANTIDAD_PROVINCIAS]
   for(i = 0; i < CANTIDAD_PROVINCIAS; i++){
     acumulador[i] /= CANTIDAD_RUBROS;
   }
+
+  imprimirAcumulador(acumulador);
 } // fin nuevoIndiceProvincial
+
+void imprimirAcumulador(float acumulador[CANTIDAD_PROVINCIAS])
+{
+  int i;
+
+  printf("%-15s%-15s", "Provincia", "Indice");
+  for(i = 0; i < CANTIDAD_PROVINCIAS; i++){
+    printf("%-15d%-15.2f\n", i, acumulador[i]);
+  }
+}
--- header_funciones.h
+++ header_funciones.h
@@ -1,6 +1,8 @@
 #ifndef HEADER_FUNCIONES_H_INCLUDED
 #define HEADER_FUNCIONES_H_INCLUDED
 
+#include "estructuras_datos.h"
+
 void nuevoIndiceProvincial(float indicesDeProvinciaPorRubro[CANTIDAD_PROVINCIAS][CANTIDAD_RUBROS]);
 
 
